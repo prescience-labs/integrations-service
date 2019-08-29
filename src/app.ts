95761ee7703b6f0192a658e3edfeda5ec31e818f
Jacob Dick <jdick@solutionreach.com>
2019-08-29 00:38:58 -0600

    add settings

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express = require('express')
 
 import { router } from './api'
+import { settings } from './config/settings'
 
 const app: express.Application = express()
 
 app.use('/', router)
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+app.listen(settings.port, () => {
+  console.log(`Example app listening on port ${settings.port}!`)
 })
