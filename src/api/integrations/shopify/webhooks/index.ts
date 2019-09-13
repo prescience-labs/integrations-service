62ce0e07f85fef510ed2dd16a28a5839cc55df6a
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-09-13 11:07:58 -0600

    added new product post

--- src/api/integrations/shopify/webhooks/index.ts
+++ src/api/integrations/shopify/webhooks/index.ts
@@ -1,6 +1,7 @@
 import { Router, Response, Request } from "express";
 import Shopify = require("shopify-api-node");
 import { settings } from "../../../../config/settings";
+import { logger } from "../../../../config/logger";
 
 export enum WebhookAddresses {
   order = '/order',
@@ -14,12 +15,12 @@ export const getFullyQualifiedWebhookAddress = (webhook: keyof typeof WebhookAdd
 const router = Router()
 
 router.post('/order', (req: Request, res: Response) => {
-  console.log('webhook!!', req.body)
+  console.log('order updated', req.body)
   const order: Shopify.IOrder = req.body as Shopify.IOrder
 })
 
 router.post('/product', (req: Request, res: Response) => {
-
+  logger.info('product updated', req.body)
 })
 
 router.get('/', (req: Request, res: Response) => {
