c5a80c5a09153609a17f11c8c385d937eb688579
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-09-13 15:13:07 -0600

    updated loggin

--- src/api/integrations/shopify/initialize.ts
+++ src/api/integrations/shopify/initialize.ts
@@ -17,16 +17,17 @@ export const updateStore = (params: IInitializeStore) => {
 
 export const refreshProducts = async (params: IInitializeStore) => {
   const products = await getStore(params).product.list()
+  logger.info('updating products')
   products.map(async product => {
-    const updatedProduct = await DB.Models.Product.findOneAndUpdate({ shopName: params.shopName, productId: product.id }, Product.serializeFromShopify(product, params.shopName))
-    logger.info('product updated', JSON.stringify(updatedProduct, null, 2))
+    const updatedProduct = await DB.Models.Product.findOneAndUpdate({ productId: product.id }, Product.serializeFromShopify(product, params.shopName))
   })
 }
 
 export const refreshOrders = async (params: IInitializeStore) => {
   const orders = await getStore(params).order.list()
+  logger.info('updating orders')
   orders.map(order => {
-    DB.Models.Order.findOneAndUpdate({ shopName: params.shopName, orderId: order.id }, Order.serializeFromShopify(order, params.shopName))
+    DB.Models.Order.findOneAndUpdate({ orderId: order.id }, Order.serializeFromShopify(order, params.shopName))
   })
 
 }
